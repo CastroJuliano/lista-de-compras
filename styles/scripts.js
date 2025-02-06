62cdd942989af80747a266da1fd29b4b80100acf
CastroJuliano <julianodecastro@gmail.com>
2025-02-06 12:49:16 -0300

    Reconfiguração de estilos da página e add tecla enter na adição de itens da lista

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,17 +1,28 @@
-document.getElementById("addItemButton").addEventListener("click", function(event) {
-   // Previne o comportamento padrão do botão de enviar (caso o botão esteja dentro de um formulário)
-  event.preventDefault();
+document.getElementById("addItemButton").addEventListener("click", function (event) {
+  event.preventDefault(); // Previne o comportamento padrão
 
+  addItem();  // Chama a função para adicionar o item
+});
+
+// Adicionando o evento para pressionar a tecla Enter
+document.getElementById("itemInput").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {  // Verifica se a tecla pressionada é "Enter"
+    event.preventDefault();  // Previne o comportamento padrão de envio do formulário
+    addItem();  // Chama a função para adicionar o item
+  }
+});
+
+function addItem() {
   const itemInput = document.getElementById("itemInput");
   const itemValue = itemInput.value.trim();
-  
+
   // Validação personalizada: se o campo estiver vazio
   if (itemValue === "") {
     // Exibe uma mensagem de erro personalizada
     alert("Por favor, insira um item para adicionar.");
     return;
   }
-  
+
   const ul = document.getElementById("shoppingList");
   const li = document.createElement("li");
 
@@ -20,29 +31,29 @@ document.getElementById("addItemButton").addEventListener("click", function(even
     <span class="itemText">${itemValue}</span>
     <button class="remove"></button>
   `;
-  
+
   // Marcar item como comprado
-  li.querySelector(".checkItem").addEventListener("change", function() {
+  li.querySelector(".checkItem").addEventListener("change", function () {
     li.classList.toggle("checked");
   });
 
   // Remover item e exibir a caixa de aviso
-  li.querySelector(".remove").addEventListener("click", function() {
+  li.querySelector(".remove").addEventListener("click", function () {
     ul.removeChild(li);
-    
+
     // Exibir a caixa de aviso
     const alertBox = document.getElementById("alertBox");
     alertBox.style.display = 'block';
-    
+
     // Esconder a caixa de aviso após 3 segundos
-    setTimeout(function() {
+    setTimeout(function () {
       alertBox.style.display = 'none';
     }, 5000);
   });
-  
+
   ul.appendChild(li);
   itemInput.value = ""; // Limpar o campo de entrada
-});
+};
 
 // Fechar a caixa de aviso manualmente
 document.getElementById("closeAlertButton").addEventListener("click", function() {
